8a0822cae6cf783ea7af411b59aee55ec6b4ca95
Nick Roberts <roberts.nicholas88@gmail.com>
2017-06-07 20:58:04 -0400

    set up the host factory which configures topshelf to enable the console application to be installed as a service.

--- DownloadSpeedCalculator/DownloadSpeedCalculator/Program.cs
+++ DownloadSpeedCalculator/DownloadSpeedCalculator/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace DownloadSpeedCalculator
 {
@@ -10,6 +13,44 @@ namespace DownloadSpeedCalculator
     {
         static void Main(string[] args)
         {
+            LogManager.Configuration.Variables["currentDate"] = DateTime.Now.Date.ToString(@"MMddyyyy");
+
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.UseNLog();
+
+                serviceConfig.Service<SpeedService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(
+                        () => new SpeedService());
+
+                    serviceInstance.WhenStarted(
+                        execute => execute.Start());
+
+                    serviceInstance.WhenStopped(
+                        execute => execute.Stop());
+
+                    serviceInstance.WhenPaused(
+                        execute => execute.Pause());
+
+                    serviceInstance.WhenContinued(
+                        execute => execute.Continue());
+                });
+
+                serviceConfig.EnableServiceRecovery(recoveryOption =>
+                {
+                    // wait 5 minutes and restart the service
+                    recoveryOption.RestartService(5);
+                });
+
+                serviceConfig.EnablePauseAndContinue();
+
+                serviceConfig.SetServiceName("DownloadSpeedCalculator");
+                serviceConfig.SetDisplayName("Download Speed Calculator");
+                serviceConfig.SetDescription(string.Format("Calculates the networks download speed every {0} minutes.", ConfigurationManager.AppSettings["serviceMinutes"].ToString()));
+
+                serviceConfig.StartAutomatically();
+            });
         }
     }
 }
---
+++ DownloadSpeedCalculator/DownloadSpeedCalculator/SpeedService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf.Logging;
+
+namespace DownloadSpeedCalculator
+{
+    public class SpeedService
+    {
+        private Timer _timer;
+        private static readonly LogWriter _log = HostLogger.Get<SpeedService>();
+
+        public bool Start()
+        {
+            return true;
+        }
+
+        public bool Stop()
+        {
+            return true;
+        }
+
+        public bool Pause()
+        {
+            return true;
+        }
+
+        public bool Continue()
+        {
+            return true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+
+        }
+    }
+}
